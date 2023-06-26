69445d5ee53312c4a8e6885094628758da0b849f
João Victor <79715612+joaoafigueira@users.noreply.github.com>
2023-06-26 20:34:30 -0300

    feat: Cohesion

---
+++ src/BestsPractices/BestsPracticesInOOP/BadPractices/Cohesion/Sale.cs
@@ -0,0 +1,17 @@
+﻿namespace BestsPracticesInOOP.BadPractices.Cohesion
+{
+    public class Sale
+    {
+        private string clientName;
+        private string clientCpf;
+        private string deliveryAddress;
+        private int cep;
+        //private Debt payment;
+        //private Product[] products;
+        private string sellersName;
+        private double sellerComission;
+    }
+
+    // This is a example of Bad Cohesion because the class above have
+    // many characteristics that do not directly concern itself
+}
---
+++ src/BestsPractices/BestsPracticesInOOP/GoodPractices/Cohesion/Sale.cs
@@ -0,0 +1,16 @@
+﻿namespace BestsPracticesInOOP.GoodPractices.Cohesion
+{
+    public class Sale
+    {
+        //private Client client;
+        //private Address address;
+        //private Debt payment;
+        //private Product[] products;
+        //private Seller seller;
+    }
+
+    // Above we have a more cohesive class, because the characteristics 
+    // that dont belong to this class went to their properly classes.
+
+    // Cohesion is a class have only the characteristicas that belongs to her
+}
