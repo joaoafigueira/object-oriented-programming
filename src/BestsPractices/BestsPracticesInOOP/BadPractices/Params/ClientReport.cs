8596611deddfc65cc25cce9444029834f5190699
João Victor <79715612+joaoafigueira@users.noreply.github.com>
2023-06-27 20:17:05 -0300

    Way to avoid too many parameters in a method

---
+++ src/BestsPractices/BestsPracticesInOOP/BadPractices/Params/ClientReport.cs
@@ -0,0 +1,20 @@
+﻿namespace BestsPracticesInOOP.BadPractices.Params
+{
+    public class ClientReport
+    {
+
+       public byte[] CustomerOrders(string clientCpf, string clientName, 
+           string userLoggedName, string registeredLoggedUser, DateTime initialDate, 
+           DateTime endDate, string reportType)
+        {
+            return null;
+        }
+
+        // The above parameters refer to classes that the system itself handles,
+        // So why not use these classes?
+
+        // The more you use disassociated params in a large quantity like above, the more coupling it creates.
+        // If a new parameter arrives one day it will be complicated to add it. Nonetheless, if a class is passed as param,
+        // The call to the method above it is going to be intact because the params no more it is going to be coupled
+    }
+}
---
+++ src/BestsPractices/BestsPracticesInOOP/GoodPractices/Params/ClientReport.cs
@@ -0,0 +1,10 @@
+﻿namespace BestsPracticesInOOP.GoodPractices.Params
+{
+    public class ClientReport
+    {
+        //public byte[] CustomerOrders(Client client, UserLogged user, DateTime initialDate,
+        //    DateTime endDate, string reportType) { }
+
+        // Now the params no more creates coupled code.
+    }
+}
