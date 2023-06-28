4a2a5f8d234a08d4e4363458dbbc44121c8b4e60
João Victor <79715612+joaoafigueira@users.noreply.github.com>
2023-06-28 20:53:40 -0300

    Alter folder and project name. Also learned what class, state, behavior and object in OOP

---
+++ src/SoftwareFundamentals/OOP/Class x Object/Home.cs
@@ -0,0 +1,33 @@
+﻿namespace OOP.Class_x_Object
+{
+    public class Home
+    {
+        public int Size { get; set; }
+        public int Floors { get; set; }
+        public decimal Value { get; set; }
+        public int numberVacancies { get; set; }
+
+        // A class represents an entity of the real World,
+        // anything that exists in the real world and you want
+        // to represent a information structure of that is a Class.
+    }
+
+    public class Object
+    {
+        public Object()
+        {
+
+            var home = new Home()
+            {
+                Size = 100,
+                Floors = 2,
+                Value = 100000,
+                numberVacancies = 3
+            };
+        }
+
+        // An object only exists in memory.
+        // An object it is a representation of a class in memory.
+    }
+}
+ 
---
+++ src/SoftwareFundamentals/OOP/OOP Pillars/StateAndBehavior.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OOP
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public DateTime Birthday { get; set; }
+
+        // A state of an object is the property that are being altered;
+
+        public int CalcuteAge()
+        {
+            var actualDate = DateTime.Now;
+            var age = actualDate.Year - Birthday.Year;
+
+            if (actualDate < Birthday.AddYears(age)) age--;
+
+            return age;
+        }
+
+        // A behavior of a class it is the possibility to manipulate  the
+        // object attributes.
+    }
+}
