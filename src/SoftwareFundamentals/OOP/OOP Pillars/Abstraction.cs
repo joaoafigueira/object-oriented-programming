5b96c246ea41da655ca1cb339ef3c72c45c81405
João Victor <79715612+joaoafigueira@users.noreply.github.com>
2023-06-29 20:58:17 -0300

    feat: Abstraction

---
+++ src/SoftwareFundamentals/OOP/OOP Pillars/Abstraction.cs
@@ -0,0 +1,21 @@
+﻿namespace OOP
+{
+    public abstract class Appliance
+    {
+        private readonly string _name;
+        private readonly int _voltage;
+
+        public Appliance(string name, int voltage)
+        {
+            _name = name;
+            _voltage = voltage;
+        }
+
+        public abstract void On();
+
+        public abstract void Off();
+
+        // The idea behind the abstract class is to have an initial 
+        // structure, where other classes that inherit from it will specialize their behavior.
+    }
+}
