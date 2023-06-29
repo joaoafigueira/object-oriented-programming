e5fbfe2046bc1e4e13b97646170b374ad4981da1
João Victor <79715612+joaoafigueira@users.noreply.github.com>
2023-06-29 20:45:36 -0300

    feat: Heritage

---
+++ src/SoftwareFundamentals/OOP/OOP Pillars/Heritage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.Intrinsics.X86;
+
+namespace OOP
+{
+    public class Employee : Person
+    {
+        public DateTime AdmissionDate { get; set; }
+        public string Register { get; set; }
+    }
+
+    //For inheritance to happen, a (child) class must be of the type of the parent class.
+    //We cannot use inheritance simply to reuse code,  as there is a
+    //semantic break (different meaning that is associated with the sense of the inheritance that is occurring).
+}
