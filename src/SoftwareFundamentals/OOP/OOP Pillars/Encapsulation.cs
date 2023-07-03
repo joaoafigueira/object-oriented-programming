c6e3124c6ac2a6e14bf2343a28631d4965fae8e7
João Victor <79715612+joaoafigueira@users.noreply.github.com>
2023-07-03 20:25:01 -0300

    Encapsulation

---
+++ src/SoftwareFundamentals/OOP/OOP Pillars/Encapsulation.cs
@@ -0,0 +1,41 @@
+﻿namespace OOP
+{
+    public class Encapsulation
+    {
+        public void ServeCoffe()
+        {
+            var express = new ExpressoMachine();
+            express.On();
+            express.PrepareCoffe();
+            express.Off();
+
+            //An Advantage of encapsulation is that the changes become transparently,
+            //who it is calling it is not going to be affected
+            //with the behavior change.
+
+            //Just the result it is important. Encapsulating behavior that are being done privately in the class.
+        }
+
+        public class ExpressoMachine
+        {
+            //With encapsulation we can hide the way something is done,
+            //giving the people who are calling the method
+            //just the result.
+
+            public void On()
+            {
+
+            }
+
+            public void PrepareCoffe()
+            {
+
+            }
+
+            public void Off()
+            {
+
+            }
+        }
+    }
+}
